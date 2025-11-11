4dbc75f923b6467ed2ca56a99b4ef3574628bfcf
Jacob M <80480036+hydrajake@users.noreply.github.com>
2025-11-11 08:07:23 -0500

    Update InstallTDAR.sh

--- tdar1/InstallTDAR.sh
+++ tdar1/InstallTDAR.sh
@@ -3,12 +3,24 @@
 # tdar (Total DAR)
 # Installs tdar
 #
-# Installs prereqs (jq)
+# Installs prereqs (tdar and jq)
+	echo "Installing Prerequisites"
+	sleep 1
+	echo "Installing jq"
+	sleep 1
 	apt install jq
+	sleep 1
+	echo "Instaling tadar"
+	sleep 1
 	wget tdar23 https://raw.githubusercontent.com/hydrajake/tdar/refs/heads/main/tdar1/tdar23
 # Makes tdar executable and moves it to /bin
+	sleep 1
+	echo "Finishing up..."
 	sudo chmod +x tdar23
 	sudo mv tdar23 /bin/tdar
+	clear
+	echo "tdar installed!"
+	sleep 1
 #
 # End of file
 #
